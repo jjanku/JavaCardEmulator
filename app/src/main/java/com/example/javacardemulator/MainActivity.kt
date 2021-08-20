0cbfdee348296968fc8df396f9ea3768aa014582
Jakub Janků <janku.jakub.jj@gmail.com>
2021-08-20 13:55:55 +0200

    ui: add simple information banner

--- app/src/main/java/com/example/javacardemulator/MainActivity.kt
+++ app/src/main/java/com/example/javacardemulator/MainActivity.kt
@@ -3,11 +3,23 @@ package com.example.javacardemulator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.javacardemulator.ui.theme.JavaCardEmulatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +29,7 @@ class MainActivity : ComponentActivity() {
             JavaCardEmulatorTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -25,14 +37,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                Icons.Default.Close,
+                contentDescription = "",
+                Modifier.size(96.dp),
+            )
+            Text(
+                textAlign = TextAlign.Center,
+                text = buildAnnotatedString {
+                    withStyle(SpanStyle(fontWeight = FontWeight.Bold)) {
+                        append("Nothing here.\n")
+                    }
+                    append(
+                        """
+                            The service runs independently of the activity
+                            whenever the screen is on.
+                        """.trimIndent()
+                    )
+                }
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     JavaCardEmulatorTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
